8792a58fc0b3c128590e17e5e7d77fe4f8066841
thanakhom <thanakom51@gmail.com>
2018-06-26 11:40:02 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class myCode {
+
+    public static void main(String[] args) {
+
+    System.out.println("Hello World.");
+    System.out.println("Thanakhom  Thongmee");
+    System.out.println("361211760025");
+    System.out.println("MIT211");
+    System.out.println("RMUTSV");
+
+    }//main
+
+
+
+
+
+
+
+
+}//class
